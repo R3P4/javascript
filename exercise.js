ffe328bdd10a164ac94b54956cffd4b65a67a874
René Pas <info@joint-creations.nl>
2022-10-20 17:02:30 +0200

    prijzen regen

---
+++ exercise.js
@@ -0,0 +1,8 @@
+const allNumbers =  [ 20 , 12 , 223.7 , 343.25 , 225.9 , 8.49 , 111.1 ];
+
+allNumbers.forEach((value, index, array) =>{
+  valueBtw = value /100 * 21 
+   allNumbers.forEach = value / 100 * 21 + value 
+
+   console.log(index,"artikel","prijs zonder btw",value, "totaal inclusief btw",allNumbers.forEach,"bedrag btw", valueBtw)
+});
